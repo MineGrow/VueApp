facb866a54de5018d13f88a3871c6a03645905f8
weektrip <weektrip@foxmail.com>
2017-12-21 18:21:49 +0800

    vue-music 初始化

---
+++ music/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app" @touchmove.prevent>
+    <m-header></m-header>
+    <tab></tab>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+    <player></player>
+  </div>
+</template>
+
+<script>
+export default {
+  data: []
+}
+</script>
+
+<style lang="stylus" scoped rel="stylesheet/stylus">
+
+</style>
+
---
+++ music/src/components/disc/disc.vue
@@ -0,0 +1,29 @@
+<template>
+  <transition name="slide">
+      <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
+  </transition>
+</template>
+
+<script>
+  export default {
+    computed: {
+      title () {
+        return 123
+      },
+      bgImage () {
+        return 222
+      }
+    },
+    data () {
+      return {
+        songs: []
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
+
+
